9699e35dfa41c1a2219ccb3b84607139e4938752
Sandeep Tiwari <sandeep15mca@gmail.com>
2021-08-27 19:30:48 +0530

    initial setup

---
+++ src/main/java/com/solvathon/incognito/IncognitoApplication.java
@@ -0,0 +1,13 @@
+package com.solvathon.incognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncognitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncognitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solvathon/incognito/IncognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvathon.incognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncognitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
